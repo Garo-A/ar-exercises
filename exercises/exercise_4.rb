06d76f8c3f6c7b7a275fc0eab10895d132d590cd
Garo-A <garo.assilian@gmail.com>
2017-08-01 15:56:17 +0000

    Finished Exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,34 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+
+# * Surrey (annual_revenue of 224000, carries women's apparel only)
+# * Whistler (annual_revenue of 1900000 carries men's apparel only)
+# * Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+
+ Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+ )
+
+ Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+ )
+
+ Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+ )
+
+ @mens_stores = Store.where(mens_apparel: true)
+
+ @mens_stores.each do |store|
+   puts "Store Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+ end
